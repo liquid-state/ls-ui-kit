d84559951888037b0a6d8cc11aba09f6919329d5
Andrey Gaas <agaas@allmax.team>
2018-03-06 11:47:13 +0700

    Updated files

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const baseDir = path.resolve(__dirname, 'src');
+
+module.exports = {
+    context: baseDir,
+    entry: './index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'uikit',
+        libraryTarget: 'commonjs2'
+    },
+    externals: [
+        'react',
+        'classnames',
+        'antd',
+        'antd/dist/antd.less',
+        function (context, request, callback) {
+            if (/.less$/.test(request) && context.includes(baseDir)) {
+                /* This is one of our less files
+                we are going to mark it external so that webpack does not process it and leaves 
+                the import in the resulting index.js
+
+                When we copy the less files we copy them with their full path relative to 
+                source, so we need to make sure the import preserves that path.
+                */ 
+                const rel = path.relative(baseDir, context);
+                const fullImport = `./${path.join(rel, request)}`;
+                callback(null, 'commonjs ' + fullImport);
+                return;
+            }
+            callback();
+        }
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.less']
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: '**/*.less'
+            }
+        ])
+    ]
+}
