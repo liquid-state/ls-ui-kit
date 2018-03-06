5da32ac39b3179b2c19ee74afa01189c8cf426f7
Andrey Gaas <agaas@allmax.team>
2018-03-06 12:23:26 +0700

    Added component Data Display / Card

--- stories/LSMobile/index.js
+++ stories/LSMobile/index.js
@@ -1,37 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 
-// General
-import Button from './General/Button';
-
 // Data Display
 import Card from './Data Display/Card';
 
-// Data Entry
-import Slider from './Data Entry/Slider';
-import Rate from './Data Entry/Rate';
-import Radio from './Data Entry/Radio';
-import Checkbox from './Data Entry/Checkbox';
-
-// Navigation
-import Steps from './Navigation/Steps'
-
 storiesOf('LS Mobile', module);
-
-storiesOf('LS Mobile/General', module)
-  .add('Button', () => <Button />);
 
 // Data Display
 storiesOf('LS Mobile/Data Display', module)
   .add('Card', () => <Card />);
-
-// Data Entry
-storiesOf('LS Mobile/Data Entry', module)
-  .add('Slider', () => <Slider />)
-  .add('Rate', () => <Rate />)
-  .add('Radio', () => <Radio />)
-  .add('Checkbox', () => <Checkbox />);
-
-// Navigation
-storiesOf('LS Mobile/Navigation', module)
-  .add('Steps', () => <Steps />);
