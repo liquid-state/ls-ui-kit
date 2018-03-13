9ed81e417c18fa5de3972a4c3396e9e483ed56c8
Andrey Gaas <agaas@allmax.team>
2018-03-13 10:23:53 +0700

    Added Table

--- stories/LSMobile/index.js
+++ stories/LSMobile/index.js
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 // Data Display
 import Card from './Data Display/Card';
 import './Data Display/MediaButton';
+import Table from './Data Display/Table';
 
 // Data Entry
 import Slider from './Data Entry/Slider';
@@ -12,7 +13,8 @@ storiesOf('LS Mobile', module);
 
 // Data Display
 storiesOf('LS Mobile/Data Display', module)
-  .add('Card', () => <Card />);
+  .add('Card', () => <Card />)
+  .add('Table', () => <Table />);
 
 // Data Entry
 storiesOf('LS Mobile/Data Entry', module)
