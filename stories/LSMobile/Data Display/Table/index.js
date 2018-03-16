f62555acc3726a4d91aedf95f829fa248e76577a
Andrey Gaas <agaas@allmax.team>
2018-03-16 09:44:46 +0700

    Data for the table are in the stories

--- stories/LSMobile/Data Display/Table/index.js
+++ stories/LSMobile/Data Display/Table/index.js
@@ -1,11 +1,137 @@
 import React, { Component, Fragment } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Icon } from 'antd';
 
 
 import Header from '../../../../src/Data Display/Table/Header';
 import Rows from '../../../../src/Data Display/Table/Rows';
-import TableGeneric from '../../../../src/Data Display/Table/TableGeneric';
+import Table from '../../../../src/Data Display/Table/Table';
 
+const dataSource = [
+  {
+    key: '1',
+    type: <Icon className="type" type="solution" />,
+    form: (
+      <div>
+        <h2 className="title">Lorem ipsum.</h2>
+        <p className="sub-title">Lorem ipsum.</p>
+      </div>
+    ),
+    address: <Icon className="status" type="check-circle-o" />,
+  },
+  {
+    key: '2',
+    type: <Icon className="type" type="solution" />,
+    form: (
+      <div>
+        <h2 className="title">Lorem ipsum.</h2>
+        <p className="sub-title">Lorem ipsum.</p>
+      </div>
+    ),
+    address: <Icon className="status" type="exclamation-circle-o" />,
+    styleType: 'warning'
+  },
+  {
+    key: '3',
+    type: <Icon className="type" type="solution" />,
+    form: (
+      <div>
+        <h2 className="title">Lorem ipsum.</h2>
+        <p className="sub-title">Lorem ipsum.</p>
+      </div>
+    ),
+    address: <Icon className="status" type="check-circle-o" />
+  },
+  {
+    key: '4',
+    type: <Icon className="type" type="solution" />,
+    form: (
+      <div>
+        <h2 className="title">Lorem ipsum.</h2>
+        <p className="sub-title">Lorem ipsum.</p>
+      </div>
+    ),
+    address: <Icon className="status" type="check-circle-o" />
+  },
+  {
+    key: '5',
+    type: <Icon className="type" type="solution" />,
+    form: (
+      <div>
+        <h2 className="title">Lorem ipsum.</h2>
+        <p className="sub-title">Lorem ipsum.</p>
+      </div>
+    ),
+    address: <Icon className="status" type="exclamation-circle-o" />,
+    styleType: 'warning'
+  },
+  {
+    key: '6',
+    type: <Icon className="type" type="solution" />,
+    form: (
+      <div>
+        <h2 className="title">Lorem ipsum.</h2>
+        <p className="sub-title">Lorem ipsum.</p>
+      </div>
+    ),
+    address: <Icon className="status" type="check-circle-o" />
+  },
+  {
+    key: '7',
+    type: <Icon className="type" type="solution" />,
+    form: (
+      <div>
+        <h2 className="title">Lorem ipsum.</h2>
+        <p className="sub-title">Lorem ipsum.</p>
+      </div>
+    ),
+    address: <Icon className="status" type="exclamation-circle-o" />,
+    styleType: 'warning'
+  },
+  {
+    key: '8',
+    type: <Icon className="type" type="solution" />,
+    form: (
+      <div>
+        <h2 className="title">Lorem ipsum.</h2>
+        <p className="sub-title">Lorem ipsum.</p>
+      </div>
+    ),
+    address: <Icon className="status" type="exclamation-circle-o" />,
+    styleType: 'warning'
+  },
+  {
+    key: '9',
+    type: <Icon className="type" type="solution" />,
+    form: (
+      <div>
+        <h2 className="title">Lorem ipsum.</h2>
+        <p className="sub-title">Lorem ipsum.</p>
+      </div>
+    ),
+    address: <Icon className="status" type="check-circle-o" />
+  }
+];
+
+const columns = [
+  {
+    title: 'Type',
+    dataIndex: 'type',
+    key: 'type',
+    width: '25%'
+  },
+  {
+    title: 'Form',
+    dataIndex: 'form',
+    key: 'form',
+    width: '54%'
+  },
+  {
+    title: 'Status',
+    dataIndex: 'address',
+    key: 'status',
+    width: '21%'
+  }
+];
 
 class TablePage extends Component {
 
@@ -29,7 +155,7 @@ class TablePage extends Component {
 
         <section className="example">
           <h3 className="ex-title">Generic Table</h3>
-          <TableGeneric />
+          <Table data={dataSource} columns={columns} />
         </section>
 
       </Fragment>
