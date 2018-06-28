44eb19b257a332507196ac99caef19e826ac4a86
agolovin <user@Mac24.local>
2018-06-28 09:56:51 +0700

    Remade the Timeline component

--- stories/AntDefault/Data Display/Timeline/BigButTimeline.jsx
+++ stories/AntDefault/Data Display/Timeline/BigButTimeline.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { Icon } from 'antd';
-import TimelineCircle from '../../../../src/components/TimelineCircle';
+// import { Icon } from 'antd';
+import BigButtonTimeline from '../../../../src/components/BigButtonTimeline';
 
 const BigButTimeline = () => (
       <section className="example">
         <h3 className="ex-title">Big But Timeline</h3>
-        <TimelineCircle type="heading" text="Create a services site 2015-09-01">
-          <Icon type="clock-circle-o" style={{ fontSize: '16px' }} />
-        </TimelineCircle>
-        <TimelineCircle type="completed" text="Solve initial network problems 2015-09-01">
-          1
-        </TimelineCircle>
-        <TimelineCircle type="process" text="Technical testing 2015-09-01">
-          2
-        </TimelineCircle>
-        <TimelineCircle noline type="waiting" text="Network problems being solved 2015-09-01">
-          3
-        </TimelineCircle>
+        <BigButtonTimeline />
       </section>
 );
 
