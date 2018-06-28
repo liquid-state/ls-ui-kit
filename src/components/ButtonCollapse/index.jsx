1f849f09dc04d528aaccd40b7a4d740310eb1baa
agolovin <user@Mac24.local>
2018-06-28 14:49:07 +0700

    Add head title for BigButtonCollapse component

--- src/components/ButtonCollapse/index.jsx
+++ src/components/ButtonCollapse/index.jsx
@@ -13,53 +13,56 @@ const customPanelStyle = {
 };
 
 const ButtonCollapse = () => (
-  <Collapse bordered={false}>
-    <Panel
-      className="ant-collapse-item-completed"
-      header="This is panel header 1"
-      key="1"
-      style={customPanelStyle}
-    >
-      <div className="el-row">
-        <Icon type="check-square-o" />
-        <div className="el-fc">Parking reminder timer</div>
-      </div>
-      <div className="el-row">
-        <Icon type="check-square-o" />
-        <div>Your appointment time is 09:40</div>
-      </div>
-    </Panel>
-    <Panel
-      className="ant-collapse-item-progress"
-      header="This is panel header 2"
-      key="2"
-      style={customPanelStyle}
-    >
-      <div className="el-row">
-        <Icon type="check-square-o" />
-        <div className="el-fc">Parking reminder timer</div>
-      </div>
-      <div className="el-row">
-        <Icon type="check-square-o" />
-        <div>Your appointment time is 09:40</div>
-      </div>
-    </Panel>
-    <Panel
-      className="ant-collapse-item-waiting"
-      header="This is panel header 3"
-      key="3"
-      style={customPanelStyle}
-    >
-      <div className="el-row">
-        <Icon type="check-square-o" />
-        <div className="el-fc">Parking reminder timer</div>
-      </div>
-      <div className="el-row">
-        <Icon type="check-square-o" />
-        <div>Your appointment time is 09:40</div>
-      </div>
-    </Panel>
-  </Collapse>
+  <div>
+    <div className="head-title">Last refreshed: 12:40</div>
+    <Collapse bordered={false}>
+      <Panel
+        className="ant-collapse-item-completed"
+        header="This is panel header 1"
+        key="1"
+        style={customPanelStyle}
+      >
+        <div className="el-row">
+          <Icon type="check-square-o" />
+          <div className="el-fc">Parking reminder timer</div>
+        </div>
+        <div className="el-row">
+          <Icon type="check-square-o" />
+          <div>Your appointment time is 09:40</div>
+        </div>
+      </Panel>
+      <Panel
+        className="ant-collapse-item-progress"
+        header="This is panel header 2"
+        key="2"
+        style={customPanelStyle}
+      >
+        <div className="el-row">
+          <Icon type="check-square-o" />
+          <div className="el-fc">Parking reminder timer</div>
+        </div>
+        <div className="el-row">
+          <Icon type="check-square-o" />
+          <div>Your appointment time is 09:40</div>
+        </div>
+      </Panel>
+      <Panel
+        className="ant-collapse-item-waiting"
+        header="This is panel header 3"
+        key="3"
+        style={customPanelStyle}
+      >
+        <div className="el-row">
+          <Icon type="check-square-o" />
+          <div className="el-fc">Parking reminder timer</div>
+        </div>
+        <div className="el-row">
+          <Icon type="check-square-o" />
+          <div>Your appointment time is 09:40</div>
+        </div>
+      </Panel>
+    </Collapse>
+  </div>
 );
 
 export default ButtonCollapse;
