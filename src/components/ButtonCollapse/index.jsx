b634b58ba7bd6bcb12b9103717720e4a3ab8aadf
agolovin <user@Mac24.local>
2018-06-28 13:02:39 +0700

    Added BigButtonCollapse component

--- src/components/ButtonCollapse/index.jsx
+++ src/components/ButtonCollapse/index.jsx
@@ -5,23 +5,60 @@ import './style.less';
 const { Panel } = Collapse;
 
 const customPanelStyle = {
-  background: '#f7f7f7',
+  background: 'transparent',
   borderRadius: 4,
-  marginBottom: 24,
+  marginTop: 24,
   border: 0,
   overflow: 'hidden',
 };
 
 const ButtonCollapse = () => (
-  <Collapse bordered={false} defaultActiveKey={['1']}>
-    <Panel header="This is panel header 1" key="1" style={customPanelStyle}>
+  <Collapse bordered={false}>
+    <Panel
+      className="ant-collapse-item-completed"
+      header="This is panel header 1"
+      key="1"
+      style={customPanelStyle}
+    >
       <div className="el-row">
         <Icon type="check-square-o" />
-        <div>Prefksopv</div>
+        <div className="el-fc">Parking reminder timer</div>
+      </div>
+      <div className="el-row">
+        <Icon type="check-square-o" />
+        <div>Your appointment time is 09:40</div>
       </div>
     </Panel>
-    <Panel header="This is panel header 2" key="2" style={customPanelStyle} />
-    <Panel header="This is panel header 3" key="3" style={customPanelStyle} />
+    <Panel
+      className="ant-collapse-item-progress"
+      header="This is panel header 2"
+      key="2"
+      style={customPanelStyle}
+    >
+      <div className="el-row">
+        <Icon type="check-square-o" />
+        <div className="el-fc">Parking reminder timer</div>
+      </div>
+      <div className="el-row">
+        <Icon type="check-square-o" />
+        <div>Your appointment time is 09:40</div>
+      </div>
+    </Panel>
+    <Panel
+      className="ant-collapse-item-waiting"
+      header="This is panel header 3"
+      key="3"
+      style={customPanelStyle}
+    >
+      <div className="el-row">
+        <Icon type="check-square-o" />
+        <div className="el-fc">Parking reminder timer</div>
+      </div>
+      <div className="el-row">
+        <Icon type="check-square-o" />
+        <div>Your appointment time is 09:40</div>
+      </div>
+    </Panel>
   </Collapse>
 );
 
