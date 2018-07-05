1e401cf99ec279e9f8c4e2f2476a29499e2776ed
Nikita <nandriukh@allmax.team>
2018-07-05 14:47:11 +0700

    small changes

--- src/components/tableChecklist/index.js
+++ src/components/tableChecklist/index.js
@@ -14,11 +14,11 @@ export default class Table extends Component {
   renderRow = () => {
     const { rows, cells } = this.props;
     return Array(rows).fill(0).map((r, i) => (
-      <React.Fragment key={i + Math.random()* 33 }>
-      <tr key={i} className="space-row">
-        {Array(cells).fill(0).map((c, index) => <td key={index} />)}
-      </tr>
-      <tr key={i + Math.random() * 50 } className="content-row">{Array(cells).fill(0).map((c, index) => <td key={index}><div className="bg-content" /></td>)}</tr>
+      <React.Fragment key={i}>
+        <tr className="space-row">
+          {Array(cells).fill(0).map((c, index) => <td key={index} />)}
+        </tr>
+        <tr className="content-row">{Array(cells).fill(0).map((c, index) => <td key={index}><div className="bg-content" /></td>)}</tr>
       </React.Fragment>
     ));
   }
@@ -27,104 +27,6 @@ export default class Table extends Component {
       <table className="checklist-table">
        <tbody>
          {this.renderRow()}
-         {/* <tr className="space-row">
-          <td />
-          <td />
-          <td />
-          <td />
-          <td />
-         </tr>
-         <tr className="content-row">
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-         </tr>
-         <tr className="space-row">
-          <td />
-          <td />
-          <td />
-          <td />
-          <td />
-         </tr>
-         <tr className="content-row">
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-         </tr>
-         <tr className="space-row">
-          <td />
-          <td />
-          <td />
-          <td />
-          <td />
-         </tr>
-         <tr className="content-row">
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-         </tr>
-         <tr className="space-row">
-          <td />
-          <td />
-          <td />
-          <td />
-          <td />
-         </tr>
-         <tr className="content-row">
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-         </tr>
-         <tr className="space-row">
-          <td />
-          <td />
-          <td />
-          <td />
-          <td />
-         </tr>
-         <tr className="content-row">
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-         </tr>
-         <tr className="space-row">
-          <td />
-          <td />
-          <td />
-          <td />
-          <td />
-         </tr>
-         <tr className="content-row">
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-         </tr>
-         <tr className="space-row">
-          <td />
-          <td />
-          <td />
-          <td />
-          <td />
-         </tr>
-         <tr className="content-row">
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-          <td><div className="bg-content" /></td>
-         </tr> */}
        </tbody>
       </table>
     );
