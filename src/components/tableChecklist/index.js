abc6159180ea48558f9b8747c7192504105a097f
Nikita <nandriukh@allmax.team>
2018-07-05 14:55:40 +0700

    SMAL CHANGES

--- src/components/tableChecklist/index.js
+++ src/components/tableChecklist/index.js
@@ -14,11 +14,20 @@ export default class Table extends Component {
   renderRow = () => {
     const { rows, cells } = this.props;
     return Array(rows).fill(0).map((r, i) => (
-      <React.Fragment key={i}>
-        <tr className="space-row">
-          {Array(cells).fill(0).map((c, index) => <td key={index} />)}
-        </tr>
-        <tr className="content-row">{Array(cells).fill(0).map((c, index) => <td key={index}><div className="bg-content" /></td>)}</tr>
+      <React.Fragment key={`${i + (Math.random() * 33)}`}>
+      <tr key={`${i + 7}_key `} className="space-row">
+        {Array(cells).fill(0).map((c, index) => <td key={`${index + (Math.random() * 5)}_key `} />)}
+      </tr>
+      <tr
+        key={`${i + (Math.random() * 50)}_key `}
+        className="content-row"
+      >
+        {Array(cells).fill(0).map((c, index) => (
+          <td key={`${index + (Math.random() * 15)}_key `}>
+            <div className="bg-content" />
+          </td>
+        ))}
+      </tr>
       </React.Fragment>
     ));
   }
