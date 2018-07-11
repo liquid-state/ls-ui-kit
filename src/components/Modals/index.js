33f63e60e9189cfc8affb21e50e67b209f935f35
Chris Hopkins <chris@liquid-state.com>
2018-07-11 13:35:18 +1000

    Ensure modals are exported correctly

--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -1,4 +1,4 @@
 export { AlertModal } from './AlertModal';
 export { PopupModal } from './PopupModal';
-export { TwoButModal } from './TwoButModal';
+export { TwoButModal as TwoButtonModal } from './TwoButModal';
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 export { default as Button } from './components/Button';
+export { default as CheckableButton } from './components/ButCheckList';
 export { default as Checkbox } from './components/Checkbox';
 export { default as FullWidthCenterImage } from './components/FullWidthCenterImage';
 export { default as MediaButton } from './components/MediaButton';
 export { default as MediaButtonGroup } from './components/MediaButtonGroup/index';
+export { AlertModal, PopupModal, TwoButtonModal } from './components/Modals';
 export { default as Pager } from './components/Pager';
 export { default as Radio } from './components/Radio';
 export { default as Rate } from './components/Rate';
 export { default as Slider } from './components/Slider';
-
