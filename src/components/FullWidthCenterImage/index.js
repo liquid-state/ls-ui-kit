022cf12ede563657a94a751c035713073ef68c1d
Chris Hopkins <chris@liquid-state.com>
2018-07-18 11:08:13 +1000

    Fix full width images missing sizing constraints

--- src/components/FullWidthCenterImage/index.js
+++ src/components/FullWidthCenterImage/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { string } from 'prop-types';
+import { string, bool } from 'prop-types';
 import cn from 'classnames';
 
 import './style.less';
 
-const FullWidthCenterImage = ({ image, className, alt }) => (
-  <div className={cn('ls-ui-kit full-width-center-image', className)}>
+const FullWidthCenterImage = ({
+  image, className, alt, borderless,
+}) => (
+  <div className={cn('ls-ui-kit full-width-center-image', className, { borderless })}>
     <img src={image} alt={alt} />
   </div>
 );
@@ -14,11 +16,13 @@ FullWidthCenterImage.propTypes = {
   image: string.isRequired,
   alt: string,
   className: string,
+  borderless: bool,
 };
 
 FullWidthCenterImage.defaultProps = {
   className: null,
   alt: '',
+  borderless: false,
 };
 
 FullWidthCenterImage.displayName = 'FullWidthCenterImage';
--- stories/AntDefault/General/FullCentralWithImage/FullCentral.jsx
+++ stories/AntDefault/General/FullCentralWithImage/FullCentral.jsx
@@ -7,8 +7,15 @@ export default class FullCentralImage extends Component {
     return (
       <section className="example">
         <h3 className="ex-title">FullWidthCenterImage</h3>
+        <h4>Default</h4>
         <div>
-          <FullWidthCenterImage image={image} />
+        <FullWidthCenterImage image={image} />
+        </div>
+
+
+        <h4>With borderless enabled</h4>
+        <div>
+        <FullWidthCenterImage image={image} borderless />
         </div>
       </section>
     );
